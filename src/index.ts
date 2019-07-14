b4f0d21491a7aaa77af28d2651b3b9ef5b8df577
Hubert Matwiejczyk <hmatwiejczyk@gmail.com>
2019-07-15 01:15:39 +0200

    add html report

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,11 @@ import { CsvFileReader } from './composition/CsvFileReader';
 import { ConsoleReport } from './reportTargets/consoleReport';
 import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
+import { HtmlReport } from "./reportTargets/htmlReport";
 
 const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const summary = new Summary(new WinsAnalysis('Arsenal'), new ConsoleReport());
+const summary = new Summary(new WinsAnalysis('Arsenal'), new HtmlReport());
 summary.buildAndPrintReport(matchReader.matches);
