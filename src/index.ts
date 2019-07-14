43f9292a5fd6a42ed4fa3eec0df535ffd5b4dc05
Hubert Matwiejczyk <hmatwiejczyk@gmail.com>
2019-07-15 00:48:19 +0200

    create console report, implementation in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
+import { MatchReader } from './composition/MatchReader';
+import { CsvFileReader } from './composition/CsvFileReader';
+import { ConsoleReport } from './reportTargets/consoleReport';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { Summary } from './Summary';
 
+const csvFileReader = new CsvFileReader('football.csv');
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
+const summary = new Summary(new WinsAnalysis('Arsenal'), new ConsoleReport());
+summary.buildAndPrintReport(matchReader.matches);
