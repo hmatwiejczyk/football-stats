0f5bbcae3a01b03f80b3500db78c1a73b756eced
Hubert Matwiejczyk <hmatwiejczyk@gmail.com>
2019-07-14 22:54:30 +0200

    add abstract calss MatchReader

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,10 +1,9 @@
 import fs from 'fs';
-import { dateStringToDate } from './utils';
-import { MatchResult } from "./MatchResult";
-
-export class CsvFileReader {
-  data: string[][] = [];
+export abstract class CsvFileReader<T> {
+  data: T[] = [];
+  abstract mapRow(row: string[]): T;
   constructor(public filename: string) {}
+
   read(): void {
     this.data = fs
       .readFileSync(this.filename, {
@@ -12,16 +11,6 @@ export class CsvFileReader {
       })
       .split('\n')
       .map((row: string): string[] => row.split(','))
-      .map(
-        (row: string[]): any => [
-          dateStringToDate(row[0]),
-          row[1],
-          row[2],
-          parseInt(row[3]),
-          parseInt(row[4]),
-          row[5] as MatchResult,
-          row[6]
-        ]
-      );
+      .map(this.mapRow);
   }
 }
